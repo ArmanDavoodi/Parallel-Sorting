a08098584968121a8aab3c7d28e7bb937a0c957d
Arman <aps4197@gmail.com>
2022-06-02 22:18:52 +0430

    added comments + deleted temp arrays in merge function

--- code/sequential/merge.h
+++ code/sequential/merge.h
@@ -7,6 +7,7 @@
 namespace seq {
     constexpr int MERGE_SORT_AD_HOC = 1; // todo
 
+    // merge two blocks of sorted arrays -> midx is the starting index of the right array
     template<typename Num>
     inline void merge(Num* arr, int lidx, int midx, int ridx) {
         int nl = midx - lidx, nr = ridx - midx + 1;
@@ -14,11 +15,13 @@ namespace seq {
         Num* right = new Num[nr];
         register int i, j, k;
 
+        // copy the two blocks into two temp arrays
         for (i = 0; i < nl; ++i)
             left[i] = arr[i + lidx];
         for (i = 0; i < nr; ++i)
             right[i] = arr[i + midx];
         
+        // merge algorithm
         i = 0;
         j = 0;
         k = lidx;
@@ -34,6 +37,7 @@ namespace seq {
             ++k;
         }
 
+        // put the remaining elements in the array
         while (i < nl) {
             arr[k] = left[i];
             ++i;
@@ -46,14 +50,19 @@ namespace seq {
             ++k;
         }
 
+        delete[] left;
+        delete[] right;
+
     }
 
+    // bottom-up implementation
     template<typename Num>
     double mergeSort(Num* arr, int N) {
         double start = omp_get_wtime();
         
         register int currentSize = MERGE_SORT_AD_HOC, i;
 
+        // ad-hoc -> sort smaller blocks using insertion sort
         if (currentSize > 1) {
             for (i = 0; i < N; i += currentSize)
                 seq::insertionSort(arr + i, i + currentSize < N ? currentSize : N - i);
@@ -64,7 +73,7 @@ namespace seq {
                 int mid = std::min(i + currentSize, N-1);
                 int right = std::min(i + 2*currentSize - 1, N - 1);
 
-                if (arr[mid - 1] > arr[mid])
+                if (arr[mid - 1] > arr[mid]) // merge two blocks if they are not already sorted
                     merge(arr, i, mid, right);
             }
         }
